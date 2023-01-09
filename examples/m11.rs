95d84cebba609e67fa5fef053fba17230e01ebb9
7ROAD\yongchao.wang <1243352372@qq.com>
2023-01-09 16:59:47 +0800

    no message

--- examples/m11.rs
+++ examples/m11.rs
@@ -2,23 +2,24 @@
 // In order to pass the tests you can add-to or change any of this code.
 
 #[derive(Debug)]
-pub struct Duration;
+pub struct Duration {
+    pub secs: u64,
+}
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        unimplemented!("s, measured in seconds: {}", s)
+        Self { secs: s }
     }
 }
 
 pub trait Planet {
+    const SECONDS_IN_YEAR: f64;
+
     fn years_during(d: &Duration) -> f64 {
-        unimplemented!(
-            "convert a duration ({:?}) to the number of years on this planet for that duration",
-            d,
-        );
+        d.secs as f64 / Self::SECONDS_IN_YEAR
     }
 }
-
+const SECONDS_IN_EARTH_YEAR: f64 = 31557600.0;
 pub struct Mercury;
 pub struct Venus;
 pub struct Earth;
@@ -28,14 +29,30 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+impl Planet for Mercury {
+    const SECONDS_IN_YEAR: f64 = 0.2408467 * SECONDS_IN_EARTH_YEAR;
+}
+impl Planet for Venus {
+    const SECONDS_IN_YEAR: f64 = 0.61519726 * SECONDS_IN_EARTH_YEAR;
+}
+impl Planet for Earth {
+    const SECONDS_IN_YEAR: f64 = 1.0 * SECONDS_IN_EARTH_YEAR;
+}
+impl Planet for Mars {
+    const SECONDS_IN_YEAR: f64 = 1.8808158 * SECONDS_IN_EARTH_YEAR;
+}
+impl Planet for Jupiter {
+    const SECONDS_IN_YEAR: f64 = 11.862615 * SECONDS_IN_EARTH_YEAR;
+}
+impl Planet for Saturn {
+    const SECONDS_IN_YEAR: f64 = 29.447498 * SECONDS_IN_EARTH_YEAR;
+}
+impl Planet for Uranus {
+    const SECONDS_IN_YEAR: f64 = 84.016846 * SECONDS_IN_EARTH_YEAR;
+}
+impl Planet for Neptune {
+    const SECONDS_IN_YEAR: f64 = 164.79132 * SECONDS_IN_EARTH_YEAR;
+}
 
 fn main() {
     println!("hello world!")
