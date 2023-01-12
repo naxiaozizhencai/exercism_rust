a0732493a1158af1fdb276acb8a2a20f34fa2768
7ROAD\yongchao.wang <1243352372@qq.com>
2023-01-12 18:45:25 +0800

    no message

--- examples/difference-of-squares.rs
+++ examples/difference-of-squares.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("hello word")
 }
+pub fn square_of_sum(n: u32) -> u32 {
+    let num = n * (n + 1) / 2;
+    num.pow(2)
+}
+
+pub fn sum_of_squares(n: u32) -> u32 {
+    (1..=n).map(|n| n.pow(2)).sum()
+}
+pub fn difference(n: u32) -> u32 {
+    square_of_sum(n) - sum_of_squares(n)
+}
