8d4ad446af0417c32b936fd97355ea00706e26f7
7ROAD\yongchao.wang <1243352372@qq.com>
2023-01-16 15:10:19 +0800

    no message

---
+++ examples/sum-of-multiples.rs
@@ -0,0 +1,7 @@
+fn main() {}
+
+pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
+    (1..limit)
+        .filter(|n| factors.iter().any(|&f| f > 0u32 && n % f == 0))
+        .sum()
+}
