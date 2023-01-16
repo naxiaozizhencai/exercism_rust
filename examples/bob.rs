e626621aaa48b814e8dd873a83dca92ee83c061c
7ROAD\yongchao.wang <1243352372@qq.com>
2023-01-16 15:20:37 +0800

    no message

---
+++ examples/bob.rs
@@ -0,0 +1,31 @@
+fn main() {}
+pub fn reply(message: &str) -> &str {
+    let trimed = message.trim();
+    if trimed.is_empty() {
+        return "Fine. Be that way!";
+    }
+
+    let is_question = trimed.ends_with("?");
+    let is_yell = trimed.contains(char::is_alphabetic) && !trimed.contains(char::is_lowercase);
+    match (is_question, is_yell) {
+        (true, true) => "Calm down, I know what I'm doing!",
+        (true, false) => "Sure.",
+        (false, true) => "Whoa, chill out!",
+        (false, false) => "Whatever.",
+    }
+}
+pub fn reply1(message: &str) -> &str {
+    // unimplemented!("have Bob reply to the incoming message: {}", message)
+    let trimed = message.trim();
+    if trimed.is_empty() {
+        return "Fine. Be that way!";
+    }
+    let is_question = trimed.ends_with("?");
+    let is_yell = trimed.contains(char::is_alphabetic) && !trimed.contains(char::is_lowercase);
+    match (is_question, is_yell) {
+        (true, true) => "Calm down, I know what I'm doing!",
+        (true, false) => "Sure.",
+        (false, true) => "Whoa, chill out!",
+        (false, false) => "Whatever.",
+    }
+}
