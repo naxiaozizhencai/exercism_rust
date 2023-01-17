0213716a4341cad62a15ebdad1671fe5c75a08cb
7ROAD\yongchao.wang <1243352372@qq.com>
2023-01-17 15:19:22 +0800

    no message

--- examples/allergies.rs
+++ examples/allergies.rs
@@ -1,1 +1,34 @@
+use strum::{EnumIter, IntoEnumIterator};
+
 fn main() {}
+pub struct Allergies {
+    score: u32,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, EnumIter)]
+pub enum Allergen {
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128,
+}
+
+impl Allergies {
+    pub fn new(score: u32) -> Self {
+        Self { score: score }
+    }
+
+    pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
+        self.score & *allergen as u32 != 0
+    }
+
+    pub fn allergies(&self) -> Vec<Allergen> {
+        Allergen::iter()
+            .filter(|a| self.is_allergic_to(a))
+            .collect()
+    }
+}
